f2ea52cc0ca7e7e8c4c50b9ef4500b9e1156071e
Ajeesh Kumar Rajan <ajeeshkumar.rajan@gmail.com>
2018-08-15 18:36:10 +0530

    Json retrieval using Volley Parsing response using Gson Binding result to Recycler View Showing the list of result in page
    All these are completed.

---
+++ WaferCountires/src/main/java/com/workaround/ajeesh/wafercodechallenge/WaferCountires/Helpers/RecyclerViewAdapter.java
@@ -0,0 +1,65 @@
+package com.workaround.ajeesh.wafercodechallenge.WaferCountires.Helpers;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.workaround.ajeesh.wafercodechallenge.WaferCountires.Modals.Countries;
+import com.workaround.ajeesh.wafercodechallenge.WaferCountires.R;
+
+
+import java.util.List;
+
+/**
+ * Package Name : com.workaround.ajeesh.wafercodechallenge.WaferCountires.Helpers
+ * Created by ajesh on 15-08-2018.
+ * Project Name : WaferCodeChallenge
+ */
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder> {
+    private static final String logName = "WFR-ADPTR-RECYCLER";
+    List<Countries> listItems;
+    Context theContext;
+
+    public RecyclerViewAdapter(List<Countries> listItems, Context theContext) {
+        this.listItems = listItems;
+        this.theContext = theContext;
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LogHelper.LogThreadId(logName, "RecyclerViewAdapter : onCreateViewHolder ");
+        View theView = LayoutInflater.from(parent.getContext()).inflate(R.layout.countires_layout_source, parent, false);
+        return new RecyclerViewHolder(theView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+        LogHelper.LogThreadId(logName, "RecyclerViewAdapter : onBindViewHolder ");
+        Countries theListItem = listItems.get(position);
+        holder.countryName.setText(theListItem.getName());
+        holder.countryCurrency.setText(theListItem.getCurrencies()[0].getName());
+        holder.countryLanguage.setText(theListItem.getLanguages()[0].getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItems.size();
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        public TextView countryName;
+        public TextView countryLanguage;
+        public TextView countryCurrency;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            countryName = itemView.findViewById(R.id.textCountryName);
+            countryCurrency = itemView.findViewById(R.id.textCountryCurrency);
+            countryLanguage = itemView.findViewById(R.id.textCountryLanguage);
+        }
+    }
+}
--- WaferCountires/src/main/java/com/workaround/ajeesh/wafercodechallenge/WaferCountires/MainActivity.java
+++ WaferCountires/src/main/java/com/workaround/ajeesh/wafercodechallenge/WaferCountires/MainActivity.java
@@ -1,15 +1,89 @@
 package com.workaround.ajeesh.wafercodechallenge.WaferCountires;
 
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.workaround.ajeesh.wafercodechallenge.WaferCountires.Helpers.LogHelper;
+import com.workaround.ajeesh.wafercodechallenge.WaferCountires.Helpers.RecyclerViewAdapter;
+import com.workaround.ajeesh.wafercodechallenge.WaferCountires.Modals.Countries;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    //Vars
     private final String logName = "WFR-CLG-MAIN";
     private final String jsonUrl = "https://restcountries.eu/rest/v2/all";
+    private List<Countries> mCountries;
+    private Gson gson;
+
+    //Widgets
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        LogHelper.LogThreadId(logName, "OnCreate: Main Activity - Wafer Coding Challenge");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mCountries = new ArrayList<>();
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setDateFormat("M/d/yy hh:mm a");
+        gson = gsonBuilder.create();
+
+        LoadCountriesDataUsingVolley();
+    }
+
+    private void LoadCountriesDataUsingVolley() {
+        LogHelper.LogThreadId(logName, "Volley call has been started");
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, jsonUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray theJsonArray = new JSONArray(response);
+                    LogHelper.LogThreadId(logName, "Total list count : " + theJsonArray.length());
+
+                    mCountries = Arrays.asList(gson.fromJson(theJsonArray.toString(), Countries[].class));
+                    adapter = new RecyclerViewAdapter(mCountries, getApplicationContext());
+                    recyclerView.setAdapter(adapter);
+                } catch (JSONException e) {
+                    LogHelper.LogThreadId(logName, "LoadCountriesDataUsingVolley Exception: " + e.getMessage());
+
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "URL returned an error : " + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
+
